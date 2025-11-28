c49b444c694e09c442150920f38160985d174bb9
pr0fix <bgx152@myy.haaga-helia.fi>
2025-11-28 22:43:35 +0200

    Add missing H-tram route to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 const PORT = 3000;
 const REALTIME_API_URL2 = "https://api.digitransit.fi/routing-data/v3/finland/";
 const REALTIME_API_URL = "https://realtime.hsl.fi/realtime/vehicle-positions/v2/hsl";
-const H_TRAM_ROUTES = ["100HE", "100HI", "100HA", "100HF"];
+const H_TRAM_ROUTES = ["100HE", "100HI", "100HA", "100HF", "100HC"];
 export { PORT, REALTIME_API_URL, REALTIME_API_URL2, H_TRAM_ROUTES };
